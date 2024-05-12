b99e8cf2c446ea137afd840ab2f3af1fa9e22470
Renato Hernandez <hostman81@hotmail.com>
2024-05-11 18:30:40 -0600

    initialized

---
+++ src/test/java/com/spacenet/cas/oauth2server/Oauth2serverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacenet.cas.oauth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2serverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
